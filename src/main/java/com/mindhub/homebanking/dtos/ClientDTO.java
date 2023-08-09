1bc2c3f8b99946d66e5fbb5edba1617a6cada229
marcoavellaneda <mavellaneda81@gmail.com>
2023-08-09 00:44:13 -0300

    first commit

---
+++ src/main/java/com/mindhub/homebanking/dtos/ClientDTO.java
@@ -0,0 +1,43 @@
+package com.mindhub.homebanking.dtos;
+
+import com.mindhub.homebanking.models.Client;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ClientDTO {
+    private Long id;
+    private String firstName, lastName, email;
+
+    private Set<AccountDTO> accounts;
+
+    public ClientDTO(Client client) {
+
+        id = client.getId();
+        firstName = client.getFirstName();
+        lastName = client.getLastName();
+        email = client.getEmail();
+        accounts= client.getAccounts().stream().map(element -> new AccountDTO(element)).collect(Collectors.toSet());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public Set<AccountDTO>getAccounts(){
+        return accounts;
+    }
+}
+
+
---
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -0,0 +1,83 @@
+package com.mindhub.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.Year;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Entity
+public class Client {
+
+    //Atributos o propiedades
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name="native", strategy = "native")
+    private Long id;
+    private String firstName, lastName, email;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "client")
+    private Set<Account> accounts  =new HashSet<>();
+
+    //Constructores
+
+    public Client(){}
+    public Client(String firstName, String lastName, String email){
+
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.email=email;
+}
+
+
+    //Métodos o comportamientos
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public Set<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void addAccount(Account account){
+        account.setClient(this);
+        this.accounts.add(account);
+    }
+}
